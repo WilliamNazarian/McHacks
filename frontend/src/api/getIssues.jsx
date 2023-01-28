e62ef4e5b412e07ab0042a466f70bd5b3e57837b
Alec Tufenkjian <alectufenkjian@gmail.com>
2023-01-28 18:28:29 -0500

    changes

---
+++ frontend/src/api/getIssues.jsx
@@ -0,0 +1,40 @@
+import React, { Suspense, useEffect, useState } from "react";
+
+import { db } from "./firebase";
+import { onValue, ref, set } from "firebase/database";
+
+function GetIssues() {
+    const [issues, setIssues] = useState([]);
+
+    useEffect(() => {
+        const query = ref(db, "issues");
+
+        return onValue(query, (snapshot) => {
+            const data = snapshot.val();
+
+            const issues = Object.values(data);
+
+            setIssues(issues)
+
+        })
+    }, []);
+
+    const add = () => {
+        set(ref(db, 'issues/issue5'), {
+            title: 'lll'
+        })
+    }
+
+    return (
+        <div>
+            {issues.map((issue, index) => (
+                // <div {...issue} key={index} />
+                <div key={index}>{issue.title}</div>
+            ))}
+
+            <button onClick={add}></button>
+        </div>
+    );
+}
+
+export default GetIssues;
