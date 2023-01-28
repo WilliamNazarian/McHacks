0546e53e83aa9320b049f814187c721b42ae3b72
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-01-28 15:46:29 -0500

    creation of the component for the organizer popup

---
+++ frontend/src/Bootstrap/componentPopupOrganizer/PopupComponentsO.jsx
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function PopupComponentsO () {
+    return (
+    <Container>
+        
+    </Container>
+
+    );
+}
