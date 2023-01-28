c9169557cb4af9958fe04941628d6eb9cf94647e
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2023-01-28 18:08:06 -0500

    Creating the accordeon for the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,43 @@
 import MyNavbar from "./Bootstrap/MyNavbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Accordion from 'react-bootstrap/Accordion';
 
 function App() {
-  return <MyNavbar/>;
+  const issues = [{
+    name: "info",
+    info: "pain"
+  },
+  {
+    name: "2",
+    info: "why"
+  },
+  {
+    name: "np",
+    info: "WHYYYYY"
+  }]
+
+  
+  return (
+    <>
+      <MyNavbar />
+      <Container style={{ width: "700px" }}>
+        <Accordion>
+          {issues.map((issue, index) => {
+            return (
+              <Accordion.Item eventKey={index + 50} key={index + 50}>
+                <Accordion.Header>{issue.name}</Accordion.Header>
+                <Accordion.Body>
+                  {issue.info}
+                </Accordion.Body>
+              </Accordion.Item>
+            )
+          })}
+        </Accordion>
+      </Container>
+    </>
+
+  );
 }
 
 export default App;
