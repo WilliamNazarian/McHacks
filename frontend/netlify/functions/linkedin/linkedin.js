8524d3b20be44df92a938d4166ce3917f7104a07
Alec Tufenkjian <alectufenkjian@gmail.com>
2023-01-28 19:29:02 -0500

    Added linked in function

--- frontend/netlify/functions/linkedin/linkedin.js
+++ frontend/netlify/functions/linkedin/linkedin.js
@@ -1,14 +1,17 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const  googleIt = require('google-it')
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    query = JSON.parse(event.body).query
+
+    return googleIt({'query': `site:linkedin.com/in intitle:"${query}"`}).then(results => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ users: results }),
+      }
+    }).catch(e => {
+      console.log("error")
+    })
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
